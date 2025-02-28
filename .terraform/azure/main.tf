dd389d2a684a7c76633b794e512ae3bd364b7e51
Leo <leo.ely@universo.univates.br>
2025-02-28 00:00:09 -0300

    Removed Terraform state management creation

--- .terraform/azure/main.tf
+++ .terraform/azure/main.tf
@@ -1,9 +1,3 @@
-module "azure-terraform-state" {
-  source                       = "./modules/tfstate"
-  location                     = var.location
-  tf_state_resource_group_name = var.tf_state_resource_group_name
-}
-
 module "azure-resource-group" {
   source   = "./modules/resource-group"
   location = var.location
--- .terraform/azure/variables.tf
+++ .terraform/azure/variables.tf
@@ -3,9 +3,3 @@ variable "location" {
   description = "Location where resources will be created"
   type        = string
 }
-
-variable "tf_state_resource_group_name" {
-  default     = "tfstate"
-  description = "Resource group name for Terraform state"
-  type        = string
-}
