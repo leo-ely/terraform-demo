ddbf472d53318173e7228699c7a18593b9cbdc00
Leo <leo.ely@universo.univates.br>
2025-03-12 13:34:16 -0300

    Updated project per name conventions

--- .terraform/aws/main.tf
+++ .terraform/aws/main.tf
@@ -4,7 +4,6 @@ module "terraform_files" {
 }
 
 module "amazon_ec2_instance" {
-  depends_on = [module.terraform_files.terraform_files_bucket_id]
   source                    = "./modules/ec2-instance"
   terraform_files_bucket_id = module.terraform_files.terraform_files_bucket_id
 }
--- .terraform/aws/modules/bootstrap/main.tf
+++ .terraform/aws/modules/bootstrap/main.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = var.region
-}
-
-module "backend" {
+module "terraform_boostrap" {
   source = "../terraform-files/"
 }
