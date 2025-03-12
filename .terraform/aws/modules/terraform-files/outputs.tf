32ae889c77a0c6192e6eb04b8bdbd35e495e5ed6
Leo <leo.ely@universo.univates.br>
2025-03-12 14:20:54 -0300

    Updated output

--- .terraform/aws/modules/terraform-files/outputs.tf
+++ .terraform/aws/modules/terraform-files/outputs.tf
@@ -1,3 +1,4 @@
 output "terraform_files_bucket_id" {
-  value = aws_s3_bucket.terraform_files[0].id
+  value = data.aws_s3_bucket.terraform_state_bucket.id == ""
+    ? aws_s3_bucket.terraform_files[0].id : data.aws_s3_bucket.terraform_state_bucket.bucket
 }
