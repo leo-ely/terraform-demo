09c3e37a918e3d4dbfe378da04c75c5801849bf2
Leo <leo.ely@universo.univates.br>
2025-03-12 14:25:57 -0300

    Added missing conditional

--- .terraform/aws/modules/terraform-files/main.tf
+++ .terraform/aws/modules/terraform-files/main.tf
@@ -12,6 +12,7 @@ resource "aws_s3_bucket" "tf_files" {
 }
 
 resource "aws_s3_bucket_versioning" "tf_files_versioning" {
+  count  = data.aws_s3_bucket.tf_bucket.id == "" ? 1 : 0
   bucket = aws_s3_bucket.tf_files[0].id
 
   versioning_configuration {
