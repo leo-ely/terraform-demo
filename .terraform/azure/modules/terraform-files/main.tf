fe800180a588fe1c56d553cee4a9c51d635c07b1
Leo <leo.ely@universo.univates.br>
2025-03-12 19:52:36 -0300

    Removed already existing resource

--- .terraform/azure/modules/terraform-files/main.tf
+++ .terraform/azure/modules/terraform-files/main.tf
@@ -1,14 +1,9 @@
-resource "azurerm_resource_group" "terraform_resource_group" {
-  location = var.location
-  name     = "terraform-resource-group"
-}
-
 resource "azurerm_storage_account" "terraform_storage_account" {
   account_replication_type = "LRS"
   account_tier             = "Standard"
-  location                 = azurerm_resource_group.terraform_resource_group.location
+  location                 = var.location
   name                     = "actionsterraformstorage"
-  resource_group_name      = azurerm_resource_group.terraform_resource_group.name
+  resource_group_name      = "terraform-resource-group"
 }
 
 resource "azurerm_storage_container" "terraform_storage_container" {
