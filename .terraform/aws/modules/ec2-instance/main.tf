fda3e18b8abdccb7ea90cfdfed2639743dd9c4ef
Leo <leo.ely@universo.univates.br>
2025-03-14 20:37:31 -0300

    Updated workflows

--- .terraform/aws/modules/ec2-instance/main.tf
+++ .terraform/aws/modules/ec2-instance/main.tf
@@ -36,7 +36,7 @@ resource "aws_s3_object" "ec2_inventory_file" {
   key    = "ec2-inventory.json"
 
   content = jsonencode({
-    ec2_instances = aws_instance.aws_test_instance.*.public_dns
+    ec2_instances = aws_instance.aws_test_instance.*.public_ip
   })
 }
 
