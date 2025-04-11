4b7f442ce7e87302e5194cf31d5260856ff28d98
Leo <leo.ely@universo.univates.br>
2025-04-11 12:20:21 -0300

    Added state locking
    Added locking to AWS and GCP (Azure locks by default)
    Enabled storage versioning on Azure
    Updated bootstrapping files

--- .terraform/aws/terraform.tf
+++ .terraform/aws/terraform.tf
@@ -5,5 +5,7 @@ terraform {
     }
   }
 
+  required_version = "~>1.11.0"
+
   backend "s3" {}
 }
--- .terraform/azure/terraform.tf
+++ .terraform/azure/terraform.tf
@@ -6,5 +6,7 @@ terraform {
     }
   }
 
+  required_version = "~>1.11.0"
+
   backend "azurerm" {}
 }
